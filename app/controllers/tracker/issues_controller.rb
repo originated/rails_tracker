c351b0795a9e4fed4ca1118e4b6118ce3d1d7da0
Mike Cifani <mcifani@gmail.com>
2013-09-28 15:51:57 -0700

    update issue controller logic, change view template for application layout

--- app/controllers/tracker/issues_controller.rb
+++ app/controllers/tracker/issues_controller.rb
@@ -6,14 +6,21 @@ module Tracker
 
     def index
       # Instantiate/setup the main applications Github config settings
-      github = Github.new
+      github  = Github.new
       @issues = github.issues.list :user => Tracker.config.user, :repo => Tracker.config.repo
     end
 
     def track_issue
+      issue_id    = params[:id]
+      issue_title = params[:title]
+      issue_body  = params[:body]
+      # Find or create a record for a user who indicates they want to track an issue
+      UsersIssues.find_or_create_by_user_id_and_issue_id(:user_id => current_user.id, :issue_id => issue_id,
+      :title => issue_title, :body => issue_body)
+      redirect_to :back
     end
 
-    def un_track_issue
+    def un_track
     end
 
   end
--- app/models/tracker/users_issues.rb
+++ app/models/tracker/users_issues.rb
@@ -1,6 +1,6 @@
 module Tracker
   class UsersIssues < ActiveRecord::Base
-    attr_accessible :active, :assignee, :body, :id, :title, :user_id
+    attr_accessible :active, :assignee, :body, :id, :title, :user_id, :issue_id
 
     validates_presence_of :user_id, :title
   end
