66e4a8de74f1c102ad4af3ea74fdad4d48778433
Mike Cifani <mcifani@gmail.com>
2013-09-30 12:27:41 -0700

    Add new method to help verifiy if user is already tracking the particular issue, update index tracking page

--- lib/tracker.rb
+++ lib/tracker.rb
@@ -2,4 +2,15 @@ require "tracker/engine"
 
 module Tracker
 
+   def tracking?(user,issue)
+     # Lookup to see if user is tracking the particular issue
+     user = User.find(user)
+     results = UsersIssues.where(:user_id => user.id, :issue_id => issue)
+     if results[0].present?
+       true
+     else
+       false
+     end
+   end
+
 end
