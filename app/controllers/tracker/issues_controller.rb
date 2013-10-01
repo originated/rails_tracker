a446dd1e7c3bcb3a27efc7c8bd366db0b6e2670b
Mike Cifani <mcifani@gmail.com>
2013-09-30 17:17:51 -0700

    render only issues that have a specific label set in the application tracker config

--- app/controllers/tracker/issues_controller.rb
+++ app/controllers/tracker/issues_controller.rb
@@ -7,7 +7,9 @@ module Tracker
     def index
       # Instantiate/setup the main applications Github config settings
       github  = Github.new
-      @issues = github.issues.list :user => Tracker.config.user, :repo => Tracker.config.repo
+      @issues = github.issues.all(:user => Tracker.config.user,
+      :repo => Tracker.config.repo,
+      :labels => Tracker.config.labels)
     end
 
     def track_issue
@@ -15,8 +17,10 @@ module Tracker
       issue_title = params[:title]
       issue_body  = params[:body]
       # Find or create a record for a user who indicates they want to track an issue
-      UsersIssues.find_or_create_by_user_id_and_issue_id(:user_id => current_user.id, :issue_id => issue_id,
-      :title => issue_title, :body => issue_body)
+      UsersIssues.find_or_create_by_user_id_and_issue_id(:user_id => current_user.id,
+      :issue_id => issue_id,
+      :title => issue_title,
+      :body => issue_body)
       redirect_to :back
     end
 
