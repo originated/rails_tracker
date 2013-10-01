44d9492ea0c3b7186c7c32e67cfec4188527a2af
Mike Cifani <mcifani@gmail.com>
2013-09-30 17:01:24 -0700

    update routes for shorter more seo friendly urls

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Tracker::Engine.routes.draw do
 
   post "track_issue/:id" => "issues#track_issue", :as => :track_issue
   post "un_track/:id" => "issues#un_track", :as => :un_track
-  get "currently_tracking" => "issues#currently_tracking", :as => :currently_tracking
-  get "resolved"  => "issues#resolved", :as => :resolved_issues
+  get "saved" => "issues#currently_tracking", :as => :saved
+  get "resolved" => "issues#resolved", :as => :resolved
 
 end
