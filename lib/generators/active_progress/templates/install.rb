d4e11a88114feccdef562831401daf690a40bf60
Mike Cifani <mcifani@gmail.com>
2013-09-30 17:18:29 -0700

    add additional config setting in init generator

--- lib/generators/active_progress/templates/install.rb
+++ lib/generators/active_progress/templates/install.rb
@@ -1,9 +1,11 @@
 # Use this setup block to configure your GitHub account
 Tracker.config do |c|
   # If the repo is public facing the password is not required.  It is recommended to store confidential
-  # information like your password in ENV settings.
-  c.user = 'mcifani'
-  c.pass = 'github_password'
-  c.repo = 'active_progress'
+  # information like your password in ENV settings. By default only issues with the label public
+  # will be displayed.
+  c.user   = 'mcifani'
+  c.pass   = 'github_password'
+  c.repo   = 'active_progress'
+  c.labels = 'public'
 end
 
