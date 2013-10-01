74742b8f48c516dc14263ef39bbd8865594a52fc
Mike Cifani <mcifani@gmail.com>
2013-10-01 14:15:10 -0700

    improvements to closed/resolved issues action and view

--- app/controllers/tracker/issues_controller.rb
+++ app/controllers/tracker/issues_controller.rb
@@ -6,10 +6,11 @@ module Tracker
 
     def index
       # Instantiate/setup the main applications Github config settings
+      # Here we display only active issues
       github  = Github.new
       @issues = github.issues.all(:user => Tracker.config.user,
-      :repo => Tracker.config.repo,
-      :labels => Tracker.config.labels)
+                                  :repo => Tracker.config.repo,
+                                  :labels => Tracker.config.labels)
     end
 
     def track_issue
@@ -18,9 +19,9 @@ module Tracker
       issue_body  = params[:body]
       # Find or create a record for a user who indicates they want to track an issue
       UsersIssues.find_or_create_by_user_id_and_issue_id(:user_id => current_user.id,
-      :issue_id => issue_id,
-      :title => issue_title,
-      :body => issue_body)
+                                                         :issue_id => issue_id,
+                                                         :title => issue_title,
+                                                         :body => issue_body)
       redirect_to :back
     end
 
@@ -36,7 +37,12 @@ module Tracker
     end
 
     def resolved
-      @resolved_issues = UsersIssues.where(:active => false)
+      # Disply issues which are currently
+      github  = Github.new
+      @issues = github.issues.all(:user => Tracker.config.user,
+                                  :repo => Tracker.config.repo,
+                                  :state => "closed",
+                                  :labels => Tracker.config.labels)
     end
 
   end
