d9c7350e6817200d7127a2ca6394cc0a08b13750
Mike Cifani <mcifani@gmail.com>
2013-10-12 11:13:01 -0700

    add new action for redirecting to back with validation check

--- app/controllers/tracker/application_controller.rb
+++ app/controllers/tracker/application_controller.rb
@@ -1,4 +1,12 @@
 module Tracker
   class ApplicationController < ActionController::Base
+
+    def redirect_to_back_if_valid
+      if request.env["HTTP_REFERER"].blank?
+        redirect_to :root
+      else
+        redirect_to :back
+      end
+    end
   end
 end
