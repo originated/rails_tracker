369b806934c38201d2398c8b14f0ef0d05f05b76
Mike Cifani <mcifani@gmail.com>
2013-10-18 18:14:42 -0700

    updates

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,1 @@
-TrackerUsersIssues.create(:title => "demo issue", :active => true, :user_id => 1, :issue_id => 12345)
+ActiveProgressUsersIssues.create(:title => "demo issue", :active => true, :user_id => 1, :issue_id => 12345)
---
+++ lib/tasks/active_progress_tasks.rake
@@ -0,0 +1,4 @@
+# desc "Explaining what the task does"
+# task :tracker do
+#   # Task goes here
+# end
